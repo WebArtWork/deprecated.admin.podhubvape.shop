a30484db9ed8706849495efcf955b71ff1316953
Narolskyi Denis <asotizianov@gmail.com>
2024-06-27 17:32:09 +0300

    added appId to translates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,17 @@ export const environment = {
 	roles: ['admin', 'agent', 'owner'],
 	url: 'https://webart.work',
 	production: true,
+	appId: 'vibescrm',
+	languages: [
+		{
+			code: 'en',
+			name: 'English',
+			origin: 'English'
+		},
+		{
+			name: 'Ukrainian',
+			origin: 'українська',
+			code: 'uk'
+		}
+	]
 };
